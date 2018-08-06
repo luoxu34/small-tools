ae77833314b4594825a1066e82ab3b459265d2da
luoxu34 <zhang_zengjie@outlook.com>
2018-08-06 16:36:25 +0800

    output more information

--- multi_host_run/v1.py
+++ multi_host_run/v1.py
@@ -9,6 +9,8 @@ Requires: fabric, compatible with fabric 1.x & 2.x
 """
 
 from fabric.api import env, run, cd, execute
+from fabric.contrib.files import exists
+
 
 from setting import LOG_PATH, HOSTS
 
@@ -19,12 +21,19 @@ env.skip_bad_hosts = True  # 跳过连不上的主机
 env.abort_on_prompts = True  # 取消交互模式
 
 
-def run_cmd(cmd):
-    with cd(LOG_PATH):
+def run_cmd(cmd, path=None):
+    title = "{0} {1} {0}".format("=" * 34, env['host'])
+    print(title)
+    if LOG_PATH and exists(LOG_PATH):
+        with cd(path):
+            run(cmd)
+    else:
         run(cmd)
+    print(title)
+    print("\n\n")
 
 
 if __name__ == '__main__':
-    _cmd = "hostname"
-    execute(run_cmd, _cmd)
+    _cmd = "pwd"
+    execute(run_cmd, _cmd, LOG_PATH)
 
